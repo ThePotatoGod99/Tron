e74a457610850c4d1e61de61e3d0d452fba10ac5
Guillaume Charron <Guillaume Charron>
2017-02-25 13:41:49 -0500

    Add survival file

---
+++ src/cybercycles/Survival.java
@@ -0,0 +1,15 @@
+package cybercycles;
+
+/**
+ * Created by Guillaume on 2017-02-25.
+ */
+public class Survival extends Snake{
+    public Survival(){
+
+    }
+
+    public void calculatePath(){
+        
+    }
+
+}
