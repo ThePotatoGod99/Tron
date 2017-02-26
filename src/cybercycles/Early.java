5b1cb9fdbd8146edd36e9f64c110f818c61d7c20
Guillaume Charron <Guillaume Charron>
2017-02-25 19:39:03 -0500

    Add early1

--- src/cybercycles/Early.java
+++ src/cybercycles/Early.java
@@ -4,4 +4,10 @@ package cybercycles;
  * Created by Guillaume on 2017-02-25.
  */
 public class Early {
+    public char calculatePath(int finalX,int finalY){
+        int snakeX = snake[selfIndice].getX();
+        int snakeY = snake[selfIndice].getY();
+
+        
+    }
 }
