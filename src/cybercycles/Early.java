4dabcae63fcfe3731e8ea7860bae95cec9244b6e
Adrien <adrienmarcotte@hotmail.com>
2017-02-26 11:12:04 -0500

    La bonne version

--- src/cybercycles/Early.java
+++ src/cybercycles/Early.java
@@ -7,36 +7,36 @@ public class Early {
 
     public static boolean calculatePath(boolean[][] map, int inix, int iniy) {
         //8 if de 2 if
-        if (map[inix][iniy - 1] == true) {
-            if (map[inix - 1][iniy - 1] == true && map[inix + 1][iniy - 1] == true) {
+        if (iniy == 0 || map[inix][iniy - 1] == true) {
+            if ((inix == 0 || iniy == 0 || map[inix - 1][iniy - 1] == true) && (inix == map.length - 1 || iniy == 0 || map[inix + 1][iniy - 1] == true)) {
                 return true;
             }
-        } else if (map[inix + 1][iniy - 1] == true) {
-            if (map[inix][iniy - 1] == true && map[inix + 1][iniy] == true) {
+        } else if (iniy == 0 || inix == map.length - 1 || map[inix + 1][iniy - 1] == true) {
+            if ((iniy == 0 || map[inix][iniy - 1] == true) && (inix == map.length - 1 || map[inix + 1][iniy] == true)) {
                 return true;
             }
-        } else if (map[inix + 1][iniy] == true) {
-            if (map[inix + 1][iniy - 1] == true && map[inix + 1][iniy + 1] == true) {
+        } else if (inix == map.length - 1 || map[inix + 1][iniy] == true) {
+            if ((inix == map.length - 1 || iniy == 0 || map[inix + 1][iniy - 1] == true) && (inix == map.length - 1 || iniy == map[0].length - 1 || map[inix + 1][iniy + 1] == true)) {
                 return true;
             }
-        } else if (map[inix + 1][iniy + 1] == true) {
-            if (map[inix][iniy] == true && map[inix][iniy + 1] == true) {
+        } else if (inix == map.length - 1 || iniy == map[0].length - 1 || map[inix + 1][iniy + 1] == true) {
+            if ((inix == map.length - 1 || map[inix + 1][iniy] == true) && (iniy == map[0].length - 1 || map[inix][iniy + 1] == true)) {
                 return true;
             }
-        } else if (map[inix][iniy + 1] == true) {
-            if (map[inix + 1][iniy + 1] == true && map[inix - 1][iniy + 1] == true) {
+        } else if (iniy == map[0].length - 1 || map[inix][iniy + 1] == true) {
+            if ((inix == map.length - 1 || iniy == map[0].length - 1 || map[inix + 1][iniy + 1] == true) && (inix == 0 || iniy == map[0].length - 1 || map[inix - 1][iniy + 1] == true)) {
                 return true;
             }
-        } else if (map[inix - 1][iniy + 1] == true) {
-            if (map[inix][iniy + 1] == true && map[inix - 1][iniy] == true) {
+        } else if (inix == 0 || iniy == map[0].length - 1 || map[inix - 1][iniy + 1] == true) {
+            if ((iniy == map[0].length - 1 || map[inix][iniy + 1] == true) && (inix == 0 || map[inix - 1][iniy] == true)) {
                 return true;
             }
-        } else if (map[inix - 1][iniy] == true) {
-            if (map[inix - 1][iniy + 1] == true && map[inix - 1][iniy - 1] == true) {
+        } else if (inix == 0 || map[inix - 1][iniy] == true) {
+            if ((inix == 0 || iniy == map[0].length - 1 || map[inix - 1][iniy + 1] == true) && (inix == 0 || iniy == 0 || map[inix - 1][iniy - 1] == true)) {
                 return true;
             }
-        } else if (map[inix - 1][iniy - 1] == true) {
-            if (map[inix - 1][iniy] == true && map[inix][iniy - 1] == true) {
+        } else if (inix == 0 || iniy == 0 || map[inix - 1][iniy - 1] == true) {
+            if ((inix == 0 || map[inix - 1][iniy] == true) && (iniy == 0 || map[inix][iniy - 1] == true)) {
                 return true;
             }
         }
