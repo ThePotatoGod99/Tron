ad4603bc209dd3b36a177b65439117c543ade7ad
simon_tran@icloud.com <tran.simon2@gmail.com>
2017-02-26 10:09:28 -0500

    asdadfgsdfg

--- src/cybercycles/AI.java
+++ src/cybercycles/AI.java
@@ -6,113 +6,117 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class AI {
+public class AI{
     //Allo
     /* Configuration */
-    public final String ROOM = "teamRocket";
+    public final String ROOM = "aaa";
     public String TEAM = "1";
-
+    
     /* Déplacement de l'A.I. */
     public final char[] directions = {'u', 'l', 'd', 'r'};
     public char direction;
-
+    
     //Informations de la map
     public int selfIndice = 0;
     public int[] allyIndice = new int[0];
     public int[] enemyIndice = new int[0];
     public boolean[][] map;
     public SnakeObject snakes[];
-
+    
     Random random = new Random();
-
+    
     int id = 0;
-
-    public AI(int i) {
+    
+    public AI(int i){
         id = i;
-        if (i == 0) {
-
-        } else if (i <= 1) {
+        if(i == 0){
+            
+        }
+        else if(i <= 1){
             TEAM = 1 + "";
-        } else {
+        }
+        else{
             TEAM = 2 + "";
         }
         System.out.println(TEAM);
     }
-
-    public void start(JSONObject config) throws JSONException {
+    
+    public void start(JSONObject config) throws JSONException{
         //Initialisation de la map
         map = new boolean[config.getInt("w")][config.getInt("h")];
-
+        
         int w, x, h, y;
-        for (int i = 0; i < config.getJSONArray("obstacles").length(); i++) {
+        for(int i = 0; i < config.getJSONArray("obstacles").length(); i++){
             w = config.getJSONArray("obstacles").getJSONObject(i).getInt("w") - 1;
             x = config.getJSONArray("obstacles").getJSONObject(i).getInt("x");
             h = config.getJSONArray("obstacles").getJSONObject(i).getInt("h") - 1;
             y = config.getJSONArray("obstacles").getJSONObject(i).getInt("y");
-
-            for (int tw = w; tw >= 0; tw--) {
-                for (int th = h; th >= 0; th--) {
-                    if (x + tw >= 0 && y + th >= 0 && x + tw < map.length && y + th < map[0].length) {
+            
+            for(int tw = w; tw >= 0; tw--){
+                for(int th = h; th >= 0; th--){
+                    if(x + tw >= 0 && y + th >= 0 && x + tw < map.length && y + th < map[0].length){
                         map[x + tw][y + th] = true;
                     }
                 }
             }
         }
-
+        
         //Initialisation des snakes
         snakes = new SnakeObject[config.getJSONArray("players").length()];
-        for (int i = 0; i < snakes.length; i++) {
+        for(int i = 0; i < snakes.length; i++){
             int temp = Integer.valueOf(config.getJSONArray("players").getJSONObject(i).getString("id")) - 1;
             snakes[temp] = new SnakeObject(config.getJSONArray("players").getJSONObject(i).getInt("x"), config.getJSONArray("players").getJSONObject(i).getInt("y"),
                     config.getJSONArray("players").getJSONObject(i).getString("id"), config.getJSONArray("players").getJSONObject(i).getString("team"));
         }
-
-        for (int i = 0; i < snakes.length; i++) {
-            if (snakes[i].getID().equals(config.getString("me"))) {
+        
+        for(int i = 0; i < snakes.length; i++){
+            if(snakes[i].getID().equals(config.getString("me"))){
                 this.selfIndice = i;
                 break;
             }
         }
-
-        for (int i = 0; i < snakes.length; i++) {
-            if (snakes[i].getID().equals(snakes[selfIndice].getID())) {
+        
+        for(int i = 0; i < snakes.length; i++){
+            if(snakes[i].getID().equals(snakes[selfIndice].getID())){
                 continue;
-            } else if (snakes[i].getTEAM().equals(snakes[selfIndice].getTEAM())) {
+            }
+            else if(snakes[i].getTEAM().equals(snakes[selfIndice].getTEAM())){
                 int[] temp = new int[allyIndice.length + 1];
-
-                for (int j = 0; j < allyIndice.length; j++) {
+                
+                for(int j = 0; j < allyIndice.length; j++){
                     temp[j] = allyIndice[j];
                 }
-
+                
                 temp[allyIndice.length] = i;
-
+                
                 allyIndice = temp;
-            } else {
+            }
+            else{
                 int[] temp = new int[enemyIndice.length + 1];
-
-                for (int j = 0; j < enemyIndice.length; j++) {
+                
+                for(int j = 0; j < enemyIndice.length; j++){
                     temp[j] = enemyIndice[j];
                 }
-
+                
                 temp[enemyIndice.length] = i;
-
+                
                 enemyIndice = temp;
             }
         }
-
-        for (int i = 0; i < snakes.length; i++) {
+        
+        for(int i = 0; i < snakes.length; i++){
             map[snakes[i].getX()][snakes[i].getY()] = true;
         }
     }
-
-    public char next(JSONArray prevMoves) throws JSONException {
+    
+    public char next(JSONArray prevMoves) throws JSONException{
         direction = 'z';
-
+        
         //Update la map et la position des snakes
-        for (int i = 0; i < prevMoves.length(); i++) {
+        for(int i = 0; i < prevMoves.length(); i++){
             int index = Integer.valueOf(prevMoves.getJSONObject(i).getString("id"));
-
-            switch (prevMoves.getJSONObject(i).getString("direction").charAt(0)) {
+            
+            switch(prevMoves.getJSONObject(i).getString("direction").charAt(0)){
                 case 'u':
                     snakes[index - 1].setY(snakes[index - 1].getY() - 1);
                     break;
@@ -126,120 +130,210 @@ public class AI {
                     snakes[index - 1].setX(snakes[index - 1].getX() + 1);
                     break;
             }
-
-            if (!snakes[i].isDead() && (snakes[i].getX() < 0 || snakes[i].getX() > map.length || snakes[i].getY() < 0 || snakes[i].getY() > map.length || map[snakes[i].getX()][snakes[i].getY()])) {
+            
+            if(!snakes[i].isDead() && (snakes[i].getX() < 0 || snakes[i].getX() > map.length || snakes[i].getY() < 0 || snakes[i].getY() > map.length || map[snakes[i].getX()][snakes[i].getY()])){
                 snakes[i].setDead();
             }
-
-            if (snakes[index - 1].getX() >= 0 && snakes[index - 1].getX() < map.length && snakes[index - 1].getY() >= 0 && snakes[index - 1].getY() < map[0].length) {
+            
+            if(snakes[index - 1].getX() >= 0 && snakes[index - 1].getX() < map.length && snakes[index - 1].getY() >= 0 && snakes[index - 1].getY() < map[0].length){
                 map[snakes[index - 1].getX()][snakes[index - 1].getY()] = true;
             }
         }
-
+        
         //Calcul pour voir si on est toujours dans le mid game
-        if(TEAM == "2"){
-            direction = (char) Survival.calculatePath(map, snakes[selfIndice].getX(), snakes[selfIndice].getY());
-        } else {
-            if (!snakes[enemyIndice[0]].isDead() && (isCloser(enemyIndice[0]) || snakes[enemyIndice[1]].isDead())) {
-                direction = Contourner.calculatePath(map, snakes[selfIndice].getX(), snakes[selfIndice].getY(), snakes[enemyIndice[0]].getX(), snakes[enemyIndice[0]].getY());
-            } else if (!snakes[enemyIndice[1]].isDead()) {
-                direction = Contourner.calculatePath(map, snakes[selfIndice].getX(), snakes[selfIndice].getY(), snakes[enemyIndice[1]].getX(), snakes[enemyIndice[1]].getY());
-            }
 
-            //Late game
-            if (Early.calculatePath(map, snakes[selfIndice].getX(), snakes[selfIndice].getY())) {
-                for (int i = 0; i < allyIndice.length; i++) {
-                    if (!snakes[i].isDead()) {
-                        if (Contourner.calculatePath(map, snakes[selfIndice].getX(), snakes[selfIndice].getY(), snakes[allyIndice[i]].getX(), snakes[allyIndice[i]].getY()) == 'z') {
-                            direction = (char) Survival.calculatePath(map, snakes[selfIndice].getX(), snakes[selfIndice].getY());
-                        } else {
-                            direction = (char) Survival.calculatePath(map, snakes[selfIndice].getX(), snakes[selfIndice].getY());
-                        }
-                    }
-                }
-            }
-
-            //Mid game
-            {
-                double distance;
-                if (!snakes[enemyIndice[0]].isDead() && (isCloser(enemyIndice[0]) || snakes[enemyIndice[1]].isDead())) {
-                    distance = getDistance(snakes[enemyIndice[0]].getX(), snakes[selfIndice].getX(), snakes[enemyIndice[0]].getY(), snakes[selfIndice].getY());
-
-                    if (distance < 1.25) {
-
-                    } else if (distance < 1.75) {
-                        //L'algorithme de early est ok pour ça
-                    } else if (distance < 2.25) {
-
-                    }
-                } else if (!snakes[enemyIndice[1]].isDead()) {
-                    distance = getDistance(snakes[enemyIndice[1]].getX(), snakes[selfIndice].getX(), snakes[enemyIndice[1]].getY(), snakes[selfIndice].getY());
-
-                    if (distance < 1.25) {
-
-                    } else if (distance < 1.75) {
-                        //L'algorithme de early est ok pour ça
-                    } else if (distance < 2.25) {
-
+        if (!snakes[enemyIndice[0]].isDead() && (isCloser(enemyIndice[0]) || snakes[enemyIndice[1]].isDead())) {
+            direction = Contourner.calculatePath(map, snakes[selfIndice].getX(), snakes[selfIndice].getY(), snakes[enemyIndice[0]].getX(), snakes[enemyIndice[0]].getY());
+        } else if(!snakes[enemyIndice[1]].isDead()){
+            direction = Contourner.calculatePath(map, snakes[selfIndice].getX(), snakes[selfIndice].getY(), snakes[enemyIndice[1]].getX(), snakes[enemyIndice[1]].getY());
+        }
+        
+        //Late game
+        if (Early.calculatePath(map, snakes[selfIndice].getX(), snakes[selfIndice].getY())) {
+            for (int i = 0; i < allyIndice.length; i++) {
+                if (!snakes[i].isDead()) {
+                    if (Contourner.calculatePath(map, snakes[selfIndice].getX(), snakes[selfIndice].getY(), snakes[allyIndice[i]].getX(), snakes[allyIndice[i]].getY()) == 'z') {
+                        direction = (char) Survival.calculatePath(map, snakes[selfIndice].getX(), snakes[selfIndice].getY());
+                    } else {
+                        direction = (char) Survival.calculatePath(map, snakes[selfIndice].getX(), snakes[selfIndice].getY());
                     }
                 }
             }
         }
+
+        //Mid game
+        {
+            double distance;
+            if (!snakes[enemyIndice[0]].isDead() && (isCloser(enemyIndice[0]) || snakes[enemyIndice[1]].isDead())) {
+                distance = getDistance(snakes[enemyIndice[0]].getX(), snakes[selfIndice].getX(), snakes[enemyIndice[0]].getY(), snakes[selfIndice].getY());
+
+                if(distance < 1.25){
+
+                } else if(distance < 1.75){
+                    //L'algorithme de early est ok pour ça
+                } else if(distance < 2.25){
+
+                }
+            } else if(!snakes[enemyIndice[1]].isDead()){
+                distance = getDistance(snakes[enemyIndice[1]].getX(), snakes[selfIndice].getX(), snakes[enemyIndice[1]].getY(), snakes[selfIndice].getY());
+
+                if(distance < 1.25){
+                    if(snakes[enemyIndice[1]].getX() == snakes[selfIndice].getX()){
+                        getBestChoice('u', 'd');
+                    } else {
+                        getBestChoice('l', 'r');
+                    }
+                } else if(distance < 1.75){
+
+                } else if(distance < 2.25){
+
+                }
+            }
+        }
+
         // imprimerMap();
 
-        System.out.println(direction + " ASDF ");
-        if(TEAM.equals("1")){
+        /*
+        int futureX = snakes[selfIndice].getX();
+        int futureY = snakes[selfIndice].getY();
+        //Calculate if move is possible
+        //  int i = 2;
+        boolean shouldStop = false;
+        
+        for(int l = 1; l <= 4; l++){
+            switch(Contourner.convertToInt(direction)){
+                //get future position
+                case 1:
+                    futureX++;
+                    break;
+        
+                case 2:
+                    futureX--;
+                    break;
+                case 3:
+                    futureY--;
+                    break;
+                case 4:
+                    futureY++;
+                    break;
+        
+            }
+    
+            if(futureX >= map.length || futureX < 0 || futureY >= map[futureX].length || futureY < 0 || map[futureX][futureY]){//Move is impossible
+    
+                direction = Contourner.convert(l);
+            }
+            else{
+                
+                break;
+            }
+        }*/
+
+        if(id == 0 || id == 1){
+            
+            
+            
+            
+            lastVerification();
+//            direction = Survival.calculatePath(map, snakes[selfIndice].getX(), snakes[selfIndice].getY());
+           // direction = Contourner.calculatePath(map, snakes[selfIndice].getX(), snakes[selfIndice].getY(), 15, 15);
+        }
+        
+        else{
             direction = Survival.calculatePath(map, snakes[selfIndice].getX(), snakes[selfIndice].getY());
         }
         return direction;//Contourner.convert(direction);
     }
-
+    
     /**
      * Fonction appelée en fin de partie.
      *
      * @param winnerID ID de l'équipe gagnante
      */
-
-    public void end(String winnerID) {
+    
+    
+    
+    public void lastVerification(){
+    
+        int futureX = snakes[selfIndice].getX();
+        int futureY = snakes[selfIndice].getY();
+        //Calculate if move is possible
+        //  int i = 2;
+        boolean shouldStop = false;
+    
+        for(int l = 1; l <= 4; l++){
+            switch(Contourner.convertToInt(direction)){
+                //get future position
+                case 1:
+                    futureX++;
+                    break;
+            
+                case 2:
+                    futureX--;
+                    break;
+                case 3:
+                    futureY--;
+                    break;
+                case 4:
+                    futureY++;
+                    break;
+            
+            }
+        
+            if(futureX >= map.length || futureX < 0 || futureY >= map[futureX].length || futureY < 0 || map[futureX][futureY]){//Move is impossible
+            
+                direction = Contourner.convert(l);
+            }
+            else{
+            
+                break;
+            }
+        }
+    }
+    public void end(String winnerID){
         System.out.println("Équipe gagnante : " + winnerID);
     }
-
-    public boolean[][] getMap() {
+    
+    public boolean[][] getMap(){
         return map;
     }
-
-    public void imprimerMap() {
-        for (int j = 0; j < map[0].length; j++) {
+    
+    public void imprimerMap(){
+        for(int j = 0; j < map[0].length; j++){
             System.out.println();
-            for (int i = 0; i < map.length; i++) {
+            for(int i = 0; i < map.length; i++){
                 boolean flag = false;
-                for (int z = 0; z < snakes.length; z++) {
-                    if (snakes[z].getX() == i && snakes[z].getY() == j) {
+                for(int z = 0; z < snakes.length; z++){
+                    if(snakes[z].getX() == i && snakes[z].getY() == j){
                         System.out.print(snakes[z].getTEAM().charAt(0));
                         flag = true;
                     }
                 }
-
-                if (!flag) {
-                    if (map[i][j]) {
+                
+                if(!flag){
+                    if(map[i][j]){
                         System.out.print("X");
-                    } else {
+                    }
+                    else{
                         System.out.print("O");
                     }
                 }
             }
         }
     }
-
-    private double getDistance(int x1, int y1, int x2, int y2) {
+    private double getDistance(int x1, int y1, int x2, int y2){
         return Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
     }
 
-
-    private boolean isCloser(int enemyIndice) {
+    
+    private boolean isCloser(int enemyIndice){
         double distance1 = Math.sqrt((snakes[selfIndice].getX() - snakes[enemyIndice].getX()) * (snakes[selfIndice].getX() - snakes[enemyIndice].getX()) + (snakes[selfIndice].getY() - snakes[enemyIndice].getY()) * (snakes[selfIndice].getY() - snakes[enemyIndice].getY()));
         double distance2 = Math.sqrt((snakes[selfIndice].getX() - snakes[enemyIndice].getX()) * (snakes[selfIndice].getX() - snakes[enemyIndice].getX()) + (snakes[selfIndice].getY() - snakes[enemyIndice].getY()) * (snakes[selfIndice].getY() - snakes[enemyIndice].getY()));
-
+        
         return (distance1 >= distance2);
+    }
+
+    private char getBestChoice(char c1, char c2){
+        return 'Z';
     }
 }
